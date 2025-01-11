e4bcfc699e2aaf2c8ba0f533ba649b0fc8cd1e74
RalleSmurf <rasmus.jacobsson.mobil@gmail.com>
2025-01-11 15:13:44 +0100

    dfg

--- Assets/OwnerComponentManager.cs
+++ Assets/OwnerComponentManager.cs
@@ -5,12 +5,13 @@ public class OwnerComponentManager : NetworkBehaviour
 {
     [SerializeField] private Camera _camera; // This is your camera, assign it in the prefab
  
-    // public override void OnNetworkSpawn()
-    // {
-    //    base.OnNetworkSpawn();
-    //    if (!IsOwner) { return; } // ALL players will read this method, only player owner will execute past this line
-    //    _camera.enabled = true; // only enable YOUR PLAYER'S camera, all others will stay disabled
-    // }
+    public override void OnNetworkSpawn()
+    {
+        _camera.enabled = false;
+       base.OnNetworkSpawn();
+       if (!IsOwner) { return; } // ALL players will read this method, only player owner will execute past this line
+       _camera.enabled = true; // only enable YOUR PLAYER'S camera, all others will stay disabled
+    }
 
     public void Update ()
     {
